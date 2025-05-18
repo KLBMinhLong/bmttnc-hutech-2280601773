57d66c7f13b9c37ef9331447916e720169ea04c2
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2025-05-18 23:20:29 +0700

    add ex03 ex04 .gitignore

--- lab-01/ex02/ex02_07.py
+++ lab-01/ex02/ex02_07.py
@@ -5,6 +5,6 @@ while True:
     if line.lower() == 'done':
         break
     lines.append(line)
-print("\nCác dòng đã nhập khi chuyển thành chũ hoa:")
+print("\nCác dòng đã nhập khi chuyển thành chữ hoa:")
 for line in lines: 
     print(line.upper())
